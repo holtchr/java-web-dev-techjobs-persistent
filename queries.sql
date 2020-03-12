fcb7b531a038821fbb656ce2ebd2e0737c4e1647
Chris H <holtchr12@gmail.com>
2020-03-11 20:14:55 -0500

    finished with assignment

--- queries.sql
+++ queries.sql
@@ -1,7 +1,22 @@
 ## Part 1: Test it with SQL
 
+Columns // Data Types
+id // int (also Primary Key)
+employer // varchar (limit 255 characters)
+name // varchar (limit 255 characters)
+skills // varchar (limit 255 characters)
+
+
 ## Part 2: Test it with SQL
+
+SELECT name FROM employer WHERE (location = "St. Louis City");
 
 ## Part 3: Test it with SQL
 
+ALTER TABLE job DROP COLUMN employer;
+
 ## Part 4: Test it with SQL
+
+SELECT DISTINCT name, description FROM skill s
+INNER JOIN job_skills js ON s.id = js.skills_id
+ORDER BY name;
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/HomeController.java
@@ -1,6 +1,13 @@
 package org.launchcode.javawebdevtechjobspersistent.controllers;
 
+import org.launchcode.javawebdevtechjobspersistent.models.Employer;
 import org.launchcode.javawebdevtechjobspersistent.models.Job;
+import org.launchcode.javawebdevtechjobspersistent.models.Skill;
+import org.launchcode.javawebdevtechjobspersistent.models.data.EmployerRepository;
+import org.launchcode.javawebdevtechjobspersistent.models.data.JobRepository;
+import org.launchcode.javawebdevtechjobspersistent.models.data.SkillRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -8,12 +15,22 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by LaunchCode
  */
 @Controller
 public class HomeController {
+
+    @Autowired
+    private EmployerRepository employerRepository;
+
+    @Autowired
+    private SkillRepository skillRepository;
+
+    @Autowired
+    private JobRepository jobRepository;
 
     @RequestMapping("")
     public String index(Model model) {
@@ -26,19 +43,34 @@ public class HomeController {
     @GetMapping("add")
     public String displayAddJobForm(Model model) {
         model.addAttribute("title", "Add Job");
+        model.addAttribute("employers", employerRepository.findAll());
+        model.addAttribute("skills", skillRepository.findAll());
         model.addAttribute(new Job());
         return "add";
     }
 
     @PostMapping("add")
     public String processAddJobForm(@ModelAttribute @Valid Job newJob,
-                                       Errors errors, Model model, @RequestParam int employerId, @RequestParam List<Integer> skills) {
+                                       Errors errors, Model model, @RequestParam int employerId, @RequestParam(required = false) List<Integer> skills) {
 
         if (errors.hasErrors()) {
             model.addAttribute("title", "Add Job");
             return "add";
         }
+        Optional optEmployer = employerRepository.findById(employerId);
+        if (optEmployer.isPresent()) {
+            Employer newEmployer = (Employer) optEmployer.get();
+            newJob.setEmployer(newEmployer);
+        } else {
+            return "add";
+        }
 
+        List<Skill> skillObjs = (List<Skill>) skillRepository.findAllById(skills);
+        //line below from suggested code on assignment #4 page, creates duplicates in job_skill join table
+        //when not commented out, when commented out the application works fine
+        //newJob.setSkills(skillObjs);
+
+        jobRepository.save(newJob);
         return "redirect:";
     }
 
